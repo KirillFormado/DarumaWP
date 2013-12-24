e07d1c679ab3c6d3776c54a89283346091b40c3d
KirillFormado <kirillformado@gmail.com>
2013-12-24 23:56:51 +0400

    Remove commented code

--- Daruma/Views/MainView.xaml.cs
+++ Daruma/Views/MainView.xaml.cs
@@ -48,72 +48,7 @@ namespace Daruma.Views
             var handler = new FirstStartHandler(_settings);
             handler.HandleFirstStart(NavigateToInfoPivotItem);  
         }
-
-        //private void DarumaContainer_OnLoaded(object sender, RoutedEventArgs e)
-        //{
-        //    LoadDaruma();
-        //    //BindDaruma();
-        //}
-
-        //private void BindDaruma()
-        //{
-        //    if(_darumaList == null)
-        //        return;
-            
-        //    var dd = _darumaList.ToLookup(d => d.Status);
-
-        //    foreach (var gr in dd)
-        //    {
-        //        var key = gr.Key;
-        //        ViewDarumaList(gr.ToList());
-        //    }
-            
-        //    //var exList = _darumaList.Where(d => d.Status == DarumaStatus.ExecutedWish);
-        //    //if (exList.Any())
-        //    //{
-        //    //    MainPivot.Items.Add(new PivotItem() {Header = "Executed Wish"});
-        //    //}
-        //}
-
-        //private void ViewDarumaList(IEnumerable<DarumaDomain> darumaList)
-        //{
-        //    DarumaStackPanel.Children.Clear();
-        //    foreach (var daruma in darumaList)
-        //    {
-        //        var grid = new Grid();
-        //        //grid.Children.Add(CreateTextBlock(daruma));
-        //        grid.Children.Add(CreateImage(daruma));
-        //        grid.Children.Add(CreateHyperlinkButton(daruma));
-        //        DarumaStackPanel.Children.Add(grid);
-        //    }
-        //}
-       
-        //private Image CreateImage(DarumaDomain daruma)
-        //{
-        //    var img = new Image
-        //    {
-        //        Height = 100,
-        //        Width = 100,
-        //        Margin = (Thickness) Resources["MarginTemplate"],
-        //        Source = new BitmapImage(daruma.ImageUri)
-        //    };
-
-        //    return img;
-        //}
-
-        //private HyperlinkButton CreateHyperlinkButton(DarumaDomain daruma)
-        //{
-        //    var hlb = new HyperlinkButton();
-
-        //    var url = string.Format("{0}?id={1}",
-        //        ViewUrlRouter.DarumaShakeViewUrl,
-        //        daruma.Id);
-
-        //    hlb.NavigateUri = new Uri(url, UriKind.Relative);
-        //    hlb.Opacity = 0;
-
-        //    return hlb;
-        //}
+        
         private void MainView_OnLoaded(object sender, RoutedEventArgs e)
         {
             InitializeSettings();
