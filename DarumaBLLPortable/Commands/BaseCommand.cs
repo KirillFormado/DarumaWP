fc2362f0b1615d414292b9203ef7b6a8162cb472
KirillFormado <kirillformado@gmail.com>
2014-03-10 21:34:54 +0400

    BaseCommand add

---
+++ DarumaBLLPortable/Commands/BaseCommand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DarumaBLLPortable.Commands
+{
+    class BaseCommand: ICommand
+    {
+        private bool _isEnabled;
+        private readonly Action _command;
+
+        public BaseCommand(Action command)
+        {
+            _command = command;
+        }
+
+        public bool IsEnabled
+        {
+            get { return _isEnabled; }
+            set
+            {
+                if (value != _isEnabled)
+                {
+                    IsEnabled = value;
+                    if (CanExecuteChanged != null)
+                    {
+                        CanExecuteChanged(this, EventArgs.Empty);
+                    }
+                }
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return IsEnabled;
+        }
+
+        public void Execute(object parameter)
+        {
+            _command();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
