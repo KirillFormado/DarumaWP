54c16c7168fd1d54b0bcff5084bfbd030e9cae6d
KirillFormado <kirillformado@gmail.com>
2014-04-09 03:41:02 +0400

    hack for json problems

--- DarumaDAL.WP/Utils/Serializer.cs
+++ DarumaDAL.WP/Utils/Serializer.cs
@@ -12,6 +12,16 @@ namespace DarumaDAL.WP.Utils
 
         public static async Task<T> Deserialize<T>(string str)
         {
+            //TODO: remove hack
+            if (str.EndsWith("}}"))
+            {
+                str = str.Remove(str.Length - 1);
+            }
+            if (str.EndsWith("\"}\"}"))
+            {
+                str = str.Remove(str.Length - 2);
+            }
+
             return await Task<T>.Factory.StartNew(() => JsonConvert.DeserializeObject<T>(str));
         }
     }
