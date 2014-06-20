18ea87d819983f2fbe7c509ec1c4eaddde462f5a
KirillFormado <kirillformado@gmail.com>
2014-06-21 03:45:12 +0400

    create base storage class

---
+++ DarumaDAL.WP/Abstraction/StorageBase.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using DarumaDAL.WP.Utils;
+
+namespace DarumaDAL.WP.Abstraction
+{
+    public abstract class StorageBase<T> where T : class
+    {
+        protected abstract string FolderName { get; }
+
+        public async Task<bool> Add(T domainObject, string id)
+        {
+            var json = await SerializeObject(domainObject);
+            
+            var folder = await GetFolderAsync();
+            var file = await folder.CreateFileAsync(id, CreationCollisionOption.FailIfExists);
+            return await WriteFile(file, json);
+        }
+
+        private async Task<bool> WriteFile(StorageFile file, string json)
+        {
+            try
+            {
+                byte[] content = Encoding.UTF8.GetBytes(json);
+                Stream stream = await file.OpenStreamForWriteAsync();
+                
+                await stream.WriteAsync(content, 0, content.Length);
+
+                stream.Flush();
+                stream.Close();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            
+        }
+
+        public async Task<T> GetById(string id)
+        {           
+            var folder = await GetFolderAsync();
+            var file = await folder.GetFileAsync(id);
+            return await DeserializeObject(file);
+        }
+
+        private async Task<StorageFolder> GetFolderAsync()
+        {
+            var folder = await ApplicationData.Current.LocalFolder.CreateFolderAsync(FolderName,
+                CreationCollisionOption.OpenIfExists);
+            return folder;
+        }        
+
+        public async virtual Task<IEnumerable<T>> ListAll()
+        {
+            var domainList = new List<T>();
+
+            var folder = await ApplicationData.Current.LocalFolder.CreateFolderAsync(FolderName,
+               CreationCollisionOption.OpenIfExists);
+
+            var files = await folder.GetFilesAsync();
+
+            foreach (var storageFile in files)
+            {
+                T domainObj = await DeserializeObject(storageFile);
+                domainList.Add(domainObj);
+            }
+                     
+            return domainList;
+        }
+
+        public async Task<bool> Update(T domainObject, string id)
+        {
+            var json = await SerializeObject(domainObject);
+            var folder = await GetFolderAsync();
+            if (folder != null)
+            {
+                var file = await folder.GetFileAsync(id);
+                if (file != null)
+                {
+                    return await WriteFile(file, json);
+                }
+            }
+
+            return false;
+        }
+
+        private async Task<string> SerializeObject(T obj)
+        {
+            var str = await Serializer.Serialize<T>(obj);
+            return str;
+        }
+
+        private async Task<T> DeserializeObject(StorageFile storageFile)
+        {
+            T domainObj;
+            IRandomAccessStream accessStream = await storageFile.OpenReadAsync();
+            using (Stream stream = accessStream.AsStreamForRead((int)accessStream.Size))
+            {
+                var content = new byte[stream.Length];
+                await stream.ReadAsync(content, 0, (int)stream.Length);
+                var text = Encoding.UTF8.GetString(content, 0, content.Length);
+                domainObj = await Serializer.Deserialize<T>(text);
+            }
+
+            return domainObj;
+        }
+
+        public async Task<bool> Delete(string id)
+        {
+            var folder = await GetFolderAsync();
+            if (folder != null)
+            {
+                var file = await folder.GetFileAsync(id);
+                if (file != null)
+                {
+                    await file.DeleteAsync(StorageDeleteOption.Default);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
--- DarumaDAL.WP/Storages/DarumaStorage.cs
+++ DarumaDAL.WP/Storages/DarumaStorage.cs
@@ -1,140 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using Windows.Storage;
-using Windows.Storage.Streams;
-using DarumaBLLPortable.Common;
-using DarumaBLLPortable.Common.Abstractions;
 using DarumaBLLPortable.Domain;
-using DarumaDAL.WP.Utils;
+using DarumaDAL.WP.Abstraction;
+using DarumaBLLPortable.Common.Abstractions;
 
 namespace DarumaDAL.WP.Storages
 {
-    public class DarumaStorage : IDarumaStorage
+    public class DarumaStorage : StorageBase<DarumaDomain>, IDarumaStorage
     {
-        private const string FolderName = "Darumas";
-
+        protected override string FolderName { get { return "Darumas"; } }
+       
         public async Task<bool> Add(DarumaDomain daruma)
         {
-            var darumaJSON = await SerializeObject(daruma.GetDTO());
-            
-            var folder = await GetFolderAsync();
-            var file = await folder.CreateFileAsync(daruma.Id.ToString(), CreationCollisionOption.FailIfExists);
-            return await WriteFile(file, darumaJSON);
-        }
-
-        private async Task<bool> WriteFile(StorageFile file, string darumaJSON)
-        {
-            try
-            {
-                byte[] content = Encoding.UTF8.GetBytes(darumaJSON);
-                Stream stream = await file.OpenStreamForWriteAsync();
-                
-                await stream.WriteAsync(content, 0, content.Length);
-
-                stream.Flush();
-                stream.Close();
-
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-            
-        }
+            return await Add(daruma, daruma.Id.ToString());            
+        }        
 
         public async Task<DarumaDomain> GetById(Guid id)
-        {           
-            var folder = await GetFolderAsync();
-            var file = await folder.GetFileAsync(id.ToString());
-            return await DeserializeObject(file);
-        }
-
-        private async Task<StorageFolder> GetFolderAsync()
         {
-            var folder = await ApplicationData.Current.LocalFolder.CreateFolderAsync(FolderName,
-                CreationCollisionOption.OpenIfExists);
-            return folder;
-        }
+            return await GetById(id.ToString());
+        }       
 
         public IEnumerable<DarumaDomain> GetByStatus(DarumaStatus status)
         {
             throw new NotImplementedException();
-        }
-
-        public async Task<IEnumerable<DarumaDomain>> ListAll()
-        {
-            var darumaList = new List<DarumaDomain>();
-
-            var folder = await ApplicationData.Current.LocalFolder.CreateFolderAsync(FolderName,
-               CreationCollisionOption.OpenIfExists);
-
-            var files = await folder.GetFilesAsync();
-
-            foreach (var storageFile in files)
-            {
-                DarumaDomain objDaruma = await DeserializeObject(storageFile);
-                darumaList.Add(objDaruma);
-            }
-
-            //May by need move order logic to the service 
-            return darumaList.OrderBy(d => d.CreateDate);
-        }
+        }       
 
         public async Task<bool> Update(DarumaDomain daruma)
         {
-            var darumaJSON = await SerializeObject(daruma.GetDTO());
-            var folder = await GetFolderAsync();
-            if (folder != null)
-            {
-                var file = await folder.GetFileAsync(daruma.Id.ToString());
-                if (file != null)
-                {
-                    return await WriteFile(file, darumaJSON);
-                }
-            }
-
-            return false;
-        }
-
-        private async Task<string> SerializeObject(DarumaDTO obj)
-        {
-            var str = await Serializer.Serialize<DarumaDTO>(obj);
-            return str;
-        }
-
-        private async Task<DarumaDomain> DeserializeObject(StorageFile storageFile)
-        {
-            DarumaDTO objDaruma;
-            IRandomAccessStream accessStream = await storageFile.OpenReadAsync();
-            using (Stream stream = accessStream.AsStreamForRead((int)accessStream.Size))
-            {
-                var content = new byte[stream.Length];
-                await stream.ReadAsync(content, 0, (int)stream.Length);
-                var text = Encoding.UTF8.GetString(content, 0, content.Length);
-                objDaruma = await Serializer.Deserialize<DarumaDTO>(text);
-            }
-
-            return new DarumaDomain(objDaruma);
-        }
+            return await Update(daruma, daruma.Id.ToString());    
+        }        
 
         public async Task<bool> Delete(Guid id)
         {
-            var folder = await GetFolderAsync();
-            if (folder != null)
-            {
-                var file = await folder.GetFileAsync(id.ToString());
-                if (file != null)
-                {
-                    await file.DeleteAsync(StorageDeleteOption.Default);
-                    return true;
-                }
-            }
-            return false;
+            return await Delete(id.ToString());
         }
     }
 }
